d9f4d0089c75a14582dff9d580d49eced15a37aa
Hjanice <hjy9399@naver.com>
2023-02-28 15:51:14 +0900

    "add updown"

---
+++ src/main/java/org/example/upDown.java
@@ -0,0 +1,16 @@
+// 소스 코드 수정 중
+
+package org.example;
+
+import java.util.Scanner;
+
+public class upDown {
+	public static void main(String[] args) {
+
+		System.out.println("updown");
+		System.out.println("updown2");
+		System.out.println("updown3");
+
+	}
+
+}
